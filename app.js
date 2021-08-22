62c26a4dbb27139207e112a2c5bcc136bd3133aa
Joseda8 <jd.montoya.s8@gmail.com>
2021-08-21 20:08:05 -0600

    Added a Procfile.

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+app.use(cors());
+
 const pokemon_list = require("./pokemon_list");
+
+var times_thesis_madalina = 0;
+
+app.get('/', (req, res) => {
+    res.send(`${times_thesis_madalina}`);
+})
+
+app.get('/plus1', (req, res) => {
+    times_thesis_madalina++;
+    res.sendStatus(200);
+})
+
+app.get('/less1', (req, res) => {
+    times_thesis_madalina--
+    res.sendStatus(200);
+})
 
 app.get('/pokemon', (req, res) => {
     res.send(pokemon_list);
